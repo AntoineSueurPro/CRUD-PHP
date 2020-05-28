8e8ab5d4214382f28e3012085a352363beddb0a7
Antoine Sueur <antoine.sueur17@gmail.com>
2020-05-28 11:24:04 +0200

    Externalisation du formulaire pour l'article

---
+++ templates/form_article.php
@@ -0,0 +1,10 @@
+
+<form method="post" action="../public/index.php?route=addArticle ?>">
+    <label for="title">Titre</label><br>
+    <input type="text" id="title" name="title"><br>
+    <label for="content">Contenu</label><br>
+    <textarea id="content" name="content"></textarea><br>
+    <label for="author">Auteur</label><br>
+    <input type="text" id="author" name="author"><br>
+    <input type="submit" value="Envoyer" id="submit" name="submit">
+</form>
