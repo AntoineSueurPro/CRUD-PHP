8e4a1ec77c63c3d203fe3d8698eca5910ae424e6
Antoine Sueur <antoine.sueur17@gmail.com>
2020-06-05 20:05:36 +0200

    Ajout de la pagination pour limiter le nombre d'articles par pages

--- templates/home.php
+++ templates/home.php
@@ -1,4 +1,21 @@
-<?php $this->title = 'Accueil'; ?>
+<?php $this->title = 'Accueil';
+$tab = array();
+foreach ($articles as $article) {
+  array_push($tab, $article);
+}
+$nb_articles = count(($articles));
+$nb_article_pages = 10;
+$nb_pages = ceil($nb_articles/$nb_article_pages);
+@$page = $_GET["page"];
+if(empty($page)) {
+  $page = 1;
+}
+$debut = ($page-1) * $nb_article_pages;
+$fin = $debut + $nb_article_pages;
+var_dump(count(($tab)));
+if($fin > count(($tab))) {
+  $fin = count(($tab));
+}?>
 <section class="home flex">
   <div class="titre-separateur flex">
     <h1 class"titre-generique flex">Mon blog</h1>
@@ -6,15 +23,21 @@
   </div>
 <div class="test flex">
   <?php
-  foreach ($articles as $article) { ?>
-  <div class="article-container flex">
-    <div class="title-date flex">
-      <div class="article-title flex"><h2><a class="link-title" href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a>
-        <span class= "date-author">- Le <?= htmlspecialchars($article->getCreatedAt()); ?>  -  par <?= htmlspecialchars($article->getAuthor());?></span></h2></div>
-    </div>
-    <div><?= '<img class="img" src ="data:image/jpeg;base64,'.base64_encode($article->getImage()).'"/>';?></div>
-    <div class="article-content flex"><?=substr($article->getContent(), 0, 550);?>... <a class="read" href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>">Lire l'article</a></p></div>
+    for($i = $debut; $i < $fin; $i++) {
+      $article = $tab[$i]; ?>
+      <div class="article-container flex">
+        <div class="title-date flex">
+          <div class="article-title flex"><h2><a class="link-title" href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a>
+            <span class= "date-author">- Le <?= htmlspecialchars($article->getCreatedAt()); ?>  -  par <?= htmlspecialchars($article->getAuthor());?></span></h2></div>
+        </div>
+        <div><?= '<img class="img" src ="data:image/jpeg;base64,'.base64_encode($article->getImage()).'"/>';?></div>
+        <div class="article-content flex"><?=substr($article->getContent(), 0, 550);?>... <a class="read" href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>">Lire l'article</a></p></div>
+      </div>
+    <?php } ?>
+    <div class="paginate">
+    <?php for ($i=1; $i <= $nb_pages; $i++) { ?>
+      <a href='?page=$i'><?= $i ?></a>
+  <?php  } ?>
   </div>
-  <?php } ?>
 </div>
 </section>
